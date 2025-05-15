69fc1dd6020b72431127baecd66e1d5800e8d44c
Md Mushfiqur Rahman <132667084+Mushfiq2007027@users.noreply.github.com>
2025-05-15 23:10:27 +0600

    fixed an error in course-controller.js

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -92,6 +92,14 @@ const checkCoursePurchaseInfo = async (req, res) => {
 			userId: studentId,
 		});
 
+		if (!studentCourses || !Array.isArray(studentCourses.courses)) {
+			// Student has no purchased courses
+			return res.status(200).json({
+				success: true,
+				data: false,
+			});
+		}
+
 		const ifStudentAlreadyBoughtCurrentCourse =
 			studentCourses.courses.findIndex((item) => item.courseId === id) > -1;
 		res.status(200).json({
